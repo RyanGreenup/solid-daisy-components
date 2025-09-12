44694b6f88c90eb316739642f4df2ba226138bfd
Ryan G <exogenesis@protonmail.com>
2025-09-12 22:23:18 +1000

    Improve Range to track signal

--- components/Range.tsx
+++ components/Range.tsx
@@ -1,5 +1,5 @@
 import { tv } from "tailwind-variants";
-import { splitProps, JSX } from "solid-js";
+import { splitProps, JSX, Accessor, Setter } from "solid-js";
 
 export const rangeVariants = tv({
   base: "range",
@@ -31,7 +31,9 @@ export const rangeVariants = tv({
 
 type RangeVariants = Parameters<typeof rangeVariants>[0];
 
-export type RangeProps = JSX.InputHTMLAttributes<HTMLInputElement> & RangeVariants;
+export type RangeProps = JSX.InputHTMLAttributes<HTMLInputElement> & RangeVariants & {
+  valueSignal?: [Accessor<number>, Setter<number>];
+};
 
 export const Range = (props: RangeProps) => {
   const [local, others] = splitProps(props, [
@@ -39,12 +41,37 @@ export const Range = (props: RangeProps) => {
     "size",
     "class",
     "type",
+    "valueSignal",
+    "onInput",
+    "onChange",
   ]);
+
+  const handleInput = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    const numValue = Number(target.value);
+    
+    if (local.valueSignal) {
+      local.valueSignal[1](numValue);
+    }
+    
+    if (local.onInput) {
+      local.onInput(e as InputEvent);
+    }
+  };
+
+  const handleChange = (e: Event) => {
+    if (local.onChange) {
+      local.onChange(e as Event & { target: HTMLInputElement });
+    }
+  };
 
   return (
     <input
       {...others}
       type="range"
+      value={local.valueSignal ? local.valueSignal[0]() : others.value}
+      onInput={handleInput}
+      onChange={handleChange}
       class={rangeVariants({
         color: local.color,
         size: local.size,
